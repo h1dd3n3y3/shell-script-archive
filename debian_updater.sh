531449edb8652e725279cb58c9dc8b60acdb0990
David Parathyras <parathyrasdavid@gmail.com>
2023-06-01 18:08:59 +0300

    Update debian_updater.sh

--- debian_updater.sh
+++ debian_updater.sh
@@ -1,5 +1,5 @@
-# All in one debian package update-upgrade-remove-cleaner
 #!/bin/bash
+# All in one debian package update-upgrade-remove-cleaner
 
 sudo apt update && echo -e "[\e[32m+\e[0m]\e[32mOK\e[0m\n"
 sudo apt upgrade -y && echo -e "[\e[32m+\e[0m]\e[32mOK\e[0m\n"
