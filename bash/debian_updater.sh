f2871738873867a837deac5ab83c2b1c1d34aa21
David Parathyras <parathyrasdavid@gmail.com>
2023-06-08 18:13:24 +0300

    Add files via upload

---
+++ bash/debian_updater.sh
@@ -0,0 +1,7 @@
+# All in one debian package update-upgrade-remove-cleaner
+#!/bin/bash
+
+sudo apt update && echo -e "[\e[32m+\e[0m]\e[32mOK\e[0m\n"
+sudo apt upgrade -y && echo -e "[\e[32m+\e[0m]\e[32mOK\e[0m\n"
+sudo apt dist-upgrade -y && echo -e "[\e[32m+\e[0m]\e[32mOK\e[0m\n"
+sudo apt autoremove -y && sudo apt clean && echo -e "[\e[32m+\e[0m]\e[32mOK\e[0m\n"
