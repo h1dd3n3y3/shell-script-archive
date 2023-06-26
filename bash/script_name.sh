f828e7d611e9ce3ea54dcf350ca59054c712ef67
David Parathyras <parathyrasdavid@gmail.com>
2023-06-27 00:52:47 +0300

    Create script_name.sh

---
+++ bash/script_name.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Seriously, read the README.md CAREFULLY!"
+echo "Openning in 3 seconds..."
+python3 -m webbrowser https://github.com/h1dd3n3y3/shell-script-archive/tree/main/bash
