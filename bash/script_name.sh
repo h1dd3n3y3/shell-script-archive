96a0a0efeedfb114e3604dcf30cca2b4c9da7ea6
David Parathyras <parathyrasdavid@gmail.com>
2023-06-27 00:59:50 +0300

    Update reminder for dummies

--- bash/script_name.sh
+++ bash/script_name.sh
@@ -1,6 +1,9 @@
 # A reminder script for dummies, lazy to read the README.md
 #!/bin/bash
 
-echo "Seriously, read the README.md CAREFULLY!"
+echo "Don't just copy & paste."
+echo "Read the README.md CAREFULLY!"
 echo "Openning in 3 seconds..."
+
+sleep 3
 python3 -m webbrowser https://github.com/h1dd3n3y3/shell-script-archive/tree/main/bash
