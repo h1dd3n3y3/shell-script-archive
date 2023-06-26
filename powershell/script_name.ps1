e17f2e62fe656d457f5cf4e5aff7f3749c8ab16d
David Parathyras <parathyrasdavid@gmail.com>
2023-06-27 01:08:15 +0300

    Update script_name.ps1

--- powershell/script_name.ps1
+++ powershell/script_name.ps1
@@ -5,4 +5,4 @@ Write-Output "Read the README.md CAREFULLY!"
 Write-Output "Openning in 3 seconds..."
 
 sleep 3
-python3 -m webbrowser https://github.com/h1dd3n3y3/shell-script-archive/tree/main/powershell
+python3 -m "webbrowser https://github.com/h1dd3n3y3/shell-script-archive/tree/main/powershell"
