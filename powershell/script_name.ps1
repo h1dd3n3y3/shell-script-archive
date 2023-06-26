dc0971544053f7627fdce244745af2282f9dbae7
David Parathyras <parathyrasdavid@gmail.com>
2023-06-27 01:27:12 +0300

    Update script_name.ps1

--- powershell/script_name.ps1
+++ powershell/script_name.ps1
@@ -4,16 +4,5 @@ Write-Output "Don't just copy & paste."
 Write-Output "Read the README.md CAREFULLY!"
 Write-Output "Openning in 3 seconds..."
 
-$osName = (Get-WmiObject Win32_OperatingSystem).Caption
-
 sleep 3
-
-if ($osName -like '*Linux*') {
-    python3 -m webbrowser https://github.com/h1dd3n3y3/shell-script-archive/tree/main/powershell
-}
-elseif ($osName -like '*Windows*') {
-    python -m webbrowser https://github.com/h1dd3n3y3/shell-script-archive/tree/main/powershell
-}
-else {
-    Write-Output "Unknown Operating System"
-}
+python -m webbrowser https://github.com/h1dd3n3y3/shell-script-archive/tree/main/powershell
