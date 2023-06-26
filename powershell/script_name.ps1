5d81cc0b8b8b7bd290285f4427efed1ecc48026d
David Parathyras <parathyrasdavid@gmail.com>
2023-06-27 01:01:46 +0300

    Create reminder script for dummies

---
+++ powershell/script_name.ps1
@@ -0,0 +1,8 @@
+# A reminder script for dummies, lazy to read the README.md
+
+echo "Don't just copy & paste."
+echo "Read the README.md CAREFULLY!"
+echo "Openning in 3 seconds..."
+
+sleep 3
+python3 -m webbrowser https://github.com/h1dd3n3y3/shell-script-archive/tree/main/powershell
