bff55185471b03fa274c07f7e3b5bc2bb6aa221a
David Parathyras <parathyrasdavid@gmail.com>
2023-11-03 15:56:44 +0200

    Update sqlplus_login.sh

--- bash/work/sqlplus_login.sh
+++ bash/work/sqlplus_login.sh
@@ -6,4 +6,4 @@ username=$(cat $conf_file | grep username | head -1 | cut -d'"' -f2)
 password=$(cat $conf_file | grep password | head -1 | cut -d'"' -f2)
 url=$(cat $conf_file | grep url | head -1 | cut -d'"' -f2 | sed 's/.*@//')
 
-sqlplus $username/"$password"@$url
+sqlplus $username/"$password"@$url <<EOF
