9c687a0e57cd34292b471244700152c8eed7faba
David Parathyras <parathyrasdavid@gmail.com>
2023-11-03 15:23:44 +0200

    Create sqlplus_login.sh

---
+++ bash/work/sqlplus_login.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+conf_file="$HOME/services/apache-tomcat-*-CORE/conf/context.xml"
+
+username=$(cat $conf_file | grep username | head -1 | cut -d'"' -f2)
+password=$(cat $conf_file | grep password | head -1 | cut -d'"' -f2)
+url=$(cat $conf_file | grep url | head -1 | cut -d'"' -f2 | sed 's/.*@//')
+
+sqlplus $username/"$password"@$url
