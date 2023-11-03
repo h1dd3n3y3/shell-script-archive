3709cd3375dc51a9134265359e0440f9d9befeb3
David Parathyras <parathyrasdavid@gmail.com>
2023-11-03 16:02:43 +0200

    Update sqlplus_login.sh

--- bash/work/sqlplus_login.sh
+++ bash/work/sqlplus_login.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -x
-
 conf_file="$HOME/services/apache-tomcat-*-CORE/conf/context.xml"
 
 username=$(cat $conf_file | grep username | head -1 | cut -d'"' -f2)
 password=$(cat $conf_file | grep password | head -1 | cut -d'"' -f2)
 url=$(cat $conf_file | grep url | head -1 | cut -d'"' -f2 | sed 's/.*@//')
 
-sqlplus $username/"$password"@$url <<EOF
+sqlplus -s $username/"$password"@$url <<EOF
+
+PAUSE
 EOF
