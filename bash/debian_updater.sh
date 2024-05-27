a9fb041ada3802696a0ee5ac6ad8bc31368974ed
David Parathyras <parathyrasdavid@gmail.com>
2024-05-27 07:22:51 +0300

    Update debian_updater.sh

--- bash/debian_updater.sh
+++ bash/debian_updater.sh
@@ -1,7 +1,14 @@
 # All in one debian package update-upgrade-remove-cleaner
 #!/bin/bash
 
-sudo apt update && echo -e "[\e[32m+\e[0m]\e[32mOK\e[0m\n"
-sudo apt upgrade -y && echo -e "[\e[32m+\e[0m]\e[32mOK\e[0m\n"
-sudo apt dist-upgrade -y && echo -e "[\e[32m+\e[0m]\e[32mOK\e[0m\n"
-sudo apt autoremove -y && sudo apt clean && echo -e "[\e[32m+\e[0m]\e[32mOK\e[0m\n"
+if ! [ "$(echo $SHELL | grep termux)" ]; then
+    sudo apt update && echo -e "[\e[32m+\e[0m]\e[32mOK\e[0m\n"
+    sudo apt upgrade -y && echo -e "[\e[32m+\e[0m]\e[32mOK\e[0m\n"
+    sudo apt dist-upgrade -y && echo -e "[\e[32m+\e[0m]\e[32mOK\e[0m\n"
+    sudo apt autoremove -y && sudo apt clean && echo -e "[\e[32m+\e[0m]\e[32mOK\e[0m\n"
+else
+    apt update && echo -e "[\e[32m+\e[0m]\e[32mOK\e[0m\n"
+    apt upgrade -y && echo -e "[\e[32m+\e[0m]\e[32mOK\e[0m\n"
+    apt dist-upgrade -y && echo -e "[\e[32m+\e[0m]\e[32mOK\e[0m\n"
+    apt autoremove -y && sudo apt clean && echo -e "[\e[32m+\e[0m]\e[32mOK\e[0m\n"
+fi
